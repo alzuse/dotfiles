387bb14869cab99d4e3889cc3c88180da21fa35b
Adam Zhang <xx@yy.zz>
2016-09-13 15:49:11 +0800

    wip

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -373,7 +373,7 @@ you should place your code here."
 
 
   ;; Font --------------------------------------------------------------------
-  ;; (add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-12"))
+  (add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-12"))
 
 
   ;; trailing-whitespace
